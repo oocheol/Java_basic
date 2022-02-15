f7b808062d3fb397f0fb674bed9cbded584775eb
oocheol <oocheol@naver.com>
2022-02-16 07:05:40 +0900

    22/02/16

--- 20220215Java기초/src/이차원배열/Ex03이차원배열실습_숙제.java
+++ 20220215Java기초/src/이차원배열/Ex03이차원배열실습_숙제.java
@@ -1,10 +1,45 @@
 package 이차원배열;
 
+import java.util.Scanner;
+
 public class Ex03이차원배열실습_숙제 {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
 		
+		int[][] arr = new int[3][5];
+
+		arr[0][0] = 1;
+		arr[0][1] = 4;
+		arr[0][2] = 3;
+		arr[0][3] = 2;
+		arr[0][4] = 1;
+		
+		arr[2][0] = 10;
+		arr[2][1] = 20;
+		arr[2][2] = 30;
+		arr[2][3] = 40;
+		arr[2][4] = 50;
+		
+		System.out.println("==채점하기==");
+		System.out.println("답을 입력하세요");
+		for (int i = 0; i<5;i++) {
+			System.out.print((i+1)+"번답 >>");
+			arr[1][i] = sc.nextInt(); 
+		}
+		System.out.println("정답확인");
+		
+		int sum = 0;
+		for (int i = 0; i<5;i++) {
+			if (arr[0][i] == arr[1][i]) {
+				System.out.print("O ");
+				sum += arr[2][i];
+			} else {
+				System.out.print("X ");
+			}
+		}
+		System.out.println("총점 : "+sum);
 		
 	}
 
